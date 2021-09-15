768d4178ce5e946c12be9a18561540fae0367fe7
Chris Hess <65086478+hesscm@users.noreply.github.com>
2021-09-15 13:40:12 -0500

    added the GuestList component

---
+++ src/components/GuestList/GuestList.js
@@ -0,0 +1,26 @@
+function GuestList(props) { //guestList
+
+    return (
+        <>
+            <h2>Guest List</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Kid's Meal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.guestList.map(guest => (
+                        <tr key={guest.id}>
+                            <td>{guest.name}</td>
+                            <td>{String(guest.kidsMeal)}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
+}
+
+export default GuestList;
