a477f1406e3073a1530dae2b61abb7fed9f5db42
Chris Hess <65086478+hesscm@users.noreply.github.com>
2021-09-15 14:13:35 -0500

    Update pool.js
    changed back the pool.js config

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -7,8 +7,8 @@ const Pool = pg.Pool;
 const pool = new Pool({
     database: 'prim-proper-props', // database name (this will change)
     host: 'localhost', // where to find the database
-    user: 'postgres',
-    password: '1211',    
+//     user: 'postgres',
+//     password: '1211',    
     port: 5432,        // port for finding the database
     max: 10,           // max number of connections for the pool
     idleTimeoutMillis: 30000 // 30 seconds before timeout/cancel query
