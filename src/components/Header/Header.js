225ea0fcb890328293a779bf34e9a5a550066bfd
Chris Hess <65086478+hesscm@users.noreply.github.com>
2021-09-15 13:24:51 -0500

    added the Footer component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,10 @@
 function Header() {
 
-    return(
-        <>
-        <header>
-        <h1>Prim Proper Props</h1>
-      </header>
-        </>
-    );
+  return (
+    <header>
+      <h1>Prim Proper Props</h1>
+    </header>
+  );
 }
 
 export default Header;
