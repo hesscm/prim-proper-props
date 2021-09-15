0a766e6b90f20707378d4a37afc7b08a827f6da0
Chris Hess <65086478+hesscm@users.noreply.github.com>
2021-09-15 13:58:37 -0500

    added the GuestForm component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import GuestList from '../GuestList/GuestList';
 import DinnerSupplies from '../DinnerSupplies/DinnerSupplies';
+import GuestForm from '../GuestForm/GuestForm';
 
 function App() {
   let [guestList, setGuestList] = useState([]);
@@ -26,7 +27,6 @@ function App() {
         console.log(err);
       })
   }
-
 
   const addGuest = () => {
     axios.post('/guests', { name: newGuestName, kidsMeal: newGuestMeal })
@@ -60,48 +60,11 @@ function App() {
       <Header />
       <h2>Party Leader</h2>
       {guestList[0] && <h3>{guestList[0].name}</h3>}
-      <h2>Add a new guest</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name
-        </label>
-        <input
-          type="text"
-          placeholder="Name"
-          value={newGuestName}
-          onChange={(evt) => setNewGuestName(evt.target.value)}
+      <GuestForm 
+        setNewGuestName ={setNewGuestName}
+        setNewGuestMeal={setNewGuestMeal}
+        handleSubmit={handleSubmit}
         />
-        <div>
-          Would this guest like a kid's meal?
-          <div >
-            <div>
-              <label>
-                <input
-                  type="radio"
-                  value={true}
-                  checked={newGuestMeal === 'true'}
-                  name="kidsMeal"
-                  onChange={(evt) => setNewGuestMeal(evt.target.value)}
-                />
-                Yes, this guest would like a Kid's Meal
-              </label>
-            </div>
-            <div>
-              <label>
-                <input
-                  type="radio"
-                  value={false}
-                  checked={newGuestMeal === 'false'}
-                  name="kidsMeal"
-                  onChange={(evt) => setNewGuestMeal(evt.target.value)}
-                />
-                No, this guest would not like a Kid's Meal
-              </label>
-            </div>
-          </div>
-        </div>
-        <button type="submit">Add Guest</button>
-      </form>
       <GuestList  guestList = {guestList} />
       <DinnerSupplies guestList = {guestList}/>
       <Footer />
